815a23e190146a2e7f85964ff7c7671183365fd2
Michael Landry <mlandryviolin@gmail.com>
2017-10-06 14:22:46 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 app = express(),
-port = process.env.PORT || 8080;
+port = process.env.PORT || 1336;
 
 console.log('starting server')
+console.log('localhost:' + port)
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
